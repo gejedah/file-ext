b2028effa354867a1c9f679e83b034d47f36b88e
Vinzh <kevin.zhong@gmail.com>
2025-12-24 16:20:36 +0700

    convert route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ConversionController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::post('/convert', [ConversionController::class, 'store'])->name('convert.store');
+Route::get('/convert/{id}', [ConversionController::class, 'show'])->name('convert.show');// status
+Route::get('/convert/{id}/download', [ConversionController::class, 'download'])->name('convert.download');
